84f7fcb603d31314b2bddb147b733992dde6db47
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-05-03 21:49:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int lista[10];
+    for(int i=0;i<10;i++){
+        cin>>lista[i];
+    }
+    int a=0;
+    cin>>a;
+    if(lista[9]>a){
+        for(int i=0;i<10;i++)
+        {
+            if(lista[i]>a){
+                for(int k=9;k>i;k--){
+                    lista[i]=a;
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
 }
+
+
+
+
+
+
